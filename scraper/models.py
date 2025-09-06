8d89da4043544ea7539b12b99e63fd7d2a769187
Tilooo <moonaviad@gmail.com>
2025-09-06 18:12:23 +0300

    1. Database table created.

--- scraper/models.py
+++ scraper/models.py
@@ -1,3 +1,14 @@
+# scraper/models.py
+
 from django.db import models
 
-# Create your models here.
+
+class Job(models.Model):
+    title = models.CharField(max_length=255)
+    company = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    posted_date = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} at {self.company}"
