9fbe91898308635d42bca0c05209a1584bf1076d
Tilooo <moonaviad@gmail.com>
2025-09-07 17:00:40 +0300

    3. The web page that displays all the jobs results that have been collected.

--- scraper/views.py
+++ scraper/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Job
 
-# Create your views here.
+
+def job_list(request):
+    jobs = Job.objects.all().order_by('-created_at') # all jobs, newest first
+
+    # the context is a dictionary that passes data to the template
+    context = {
+        'jobs': jobs
+    }
+
+    return render(request, 'scraper/job_list.html', context)
