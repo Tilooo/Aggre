e42dbf996b1219ce809cce61124995d034dddced
Tilooo <moonaviad@gmail.com>
2025-09-10 00:29:50 +0300

    8. Integrated Paginate the Job List view.

--- scraper/views.py
+++ scraper/views.py
@@ -3,21 +3,30 @@
 from django.shortcuts import render
 from .models import Job
 from django.db.models import Q
+from django.core.paginator import Paginator
+
 
 def job_list(request):
     search_query = request.GET.get('q', '')
 
-    # the newest first.
-    jobs = Job.objects.all().order_by('-created_at')
+    page_number = request.GET.get('page', 1)
+
+    jobs_list = Job.objects.all().order_by('-created_at')
 
     if search_query:
-        jobs = jobs.filter(
+        jobs_list = jobs_list.filter(
             Q(title__icontains=search_query) |
             Q(company__icontains=search_query)
         )
 
+    # Pagination Logic
+    paginator = Paginator(jobs_list, 15)
+
+    page_obj = paginator.get_page(page_number)
+
+
     context = {
-        'jobs': jobs,
+        'jobs_page': page_obj,
         'search_query': search_query,
     }
 
