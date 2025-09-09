55dfce5b34ab1caf1a65d1c5632cee2cb4a97272
Tilooo <moonaviad@gmail.com>
2025-09-10 00:12:37 +0300

    7. To handle the Search Logic.

--- scraper/views.py
+++ scraper/views.py
@@ -1,13 +1,24 @@
+# scraper/views.py
+
 from django.shortcuts import render
 from .models import Job
-
+from django.db.models import Q
 
 def job_list(request):
-    jobs = Job.objects.all().order_by('-created_at') # all jobs, newest first
+    search_query = request.GET.get('q', '')
 
-    # the context is a dictionary that passes data to the template
+    # the newest first.
+    jobs = Job.objects.all().order_by('-created_at')
+
+    if search_query:
+        jobs = jobs.filter(
+            Q(title__icontains=search_query) |
+            Q(company__icontains=search_query)
+        )
+
     context = {
-        'jobs': jobs
+        'jobs': jobs,
+        'search_query': search_query,
     }
 
     return render(request, 'scraper/job_list.html', context)
